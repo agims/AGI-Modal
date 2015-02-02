3e850e05a6370431c37599cbc9b1af8f05608b37
Chris G. Clapp <chris@chisosdesigns.com>
2015-02-02 11:17:45 -0700

    Add in options for Terlingua

--- inc/checks.php
+++ inc/checks.php
@@ -1,6 +1,7 @@
 <?php
 	
 	$needed_options = array(
+		'agi_modal_use_header'			=> TRUE,
 		'agi_modal_title'				=> 'Get More Information',
 		'agi_modal_title_size'			=> 'h3',
 		'agi_modal_use_subtitle'		=> FALSE,
@@ -9,11 +10,13 @@
 		'agi_modal_using_shortcode'		=> TRUE,
 		'agi_modal_shortcode'			=> '',
 		'agi_modal_html'				=> '',
+		'agi_modal_redirect_links'		=> TRUE,
 		'agi_modal_on_pages'			=> TRUE,
 		'agi_modal_number_of_pages'		=> '3',
 		'agi_modal_on_posts'			=> TRUE,
 		'agi_modal_number_of_posts'		=> '1',
 		'agi_modal_reset_time'			=> '10',
+		'agi_modal_number_of_views'		=> '3',
 		'agi_modal_hook'				=> '#agi-modal-hook',
 		'agi_modal_hook_percent'		=> '100',
 		'agi_modal_include_hook_el'		=> TRUE,
--- inc/options-page.php
+++ inc/options-page.php
@@ -38,82 +38,93 @@ function agi_modal_option_page() {
 			<h3>Modal Look</h3>
 			<table class="form-table">
 				<tbody>
-					<tr id="title">
+					<tr id="use_header">
 						<th scope="row">
-							<label for="agi_modal_title">Title</label>
+							<label for="agi_modal_use_header">Use Header</label>
 						</th>
 						<td>
-							<input name="agi_modal_title" id="agi_modal_title" type="text" value="<?=$agi_modal_title?>" class="regular-text ">
+							<?php $checked = ($agi_modal_use_header ? 'checked' : ''); ?>
+							<input name="agi_modal_use_header" id="agi_modal_use_header" type="checkbox" <?=$checked?>>
 						</td>
 					</tr>
-					<tr id="title-size">
-						<th scope="row">
-							<label for="agi_modal_title">Title Size</label>
-						</th>
-						<td>
-							<select id="agi_modal_title_size" name="agi_modal_title_size">
-							<?php
-								$title_size_options = array(
-									'h2', 'h3', 'h4', 'h5'
-								);
-								
-								
-								foreach($title_size_options as $title_option) {
-									if($agi_modal_title_size == $title_option) {
-										$selected = " selected='selected'";
-									} else {
-										$selected = "";
+					<div id="header-info">
+						<tr id="title">
+							<th scope="row">
+								<label for="agi_modal_title">Title</label>
+							</th>
+							<td>
+								<input name="agi_modal_title" id="agi_modal_title" type="text" value="<?=$agi_modal_title?>" class="regular-text ">
+							</td>
+						</tr>
+						<tr id="title-size">
+							<th scope="row">
+								<label for="agi_modal_title">Title Size</label>
+							</th>
+							<td>
+								<select id="agi_modal_title_size" name="agi_modal_title_size">
+								<?php
+									$title_size_options = array(
+										'h2', 'h3', 'h4', 'h5'
+									);
+									
+									
+									foreach($title_size_options as $title_option) {
+										if($agi_modal_title_size == $title_option) {
+											$selected = " selected='selected'";
+										} else {
+											$selected = "";
+										}
+										echo '<option value="' . $title_option . '" ' . $selected . '>&lt;' . $title_option . '&gt;</option>' . "\n";
 									}
-									echo '<option value="' . $title_option . '" ' . $selected . '>&lt;' . $title_option . '&gt;</option>' . "\n";
-								}
-								
-							?>
-							</select>
-						</td>
-					</tr>
-					<tr id="use-subtitle">
-						<th scope="row">
-							<label for="agi_modal_use_subtitle">Use Subtitle?</label>
-						</th>
-						<td>
-							<?php $checked = ($agi_modal_use_subtitle ? 'checked' : ''); ?>
-							<input name="agi_modal_use_subtitle" id="agi_modal_use_subtitle" type="checkbox" <?=$checked?>>
-						</td>
-					</tr>
-					<tr id="subtitle">
-						<th scope="row">
-							<label for="agi_modal_subtitle">Subtitle</label>
-						</th>
-						<td>
-							<input name="agi_modal_subtitle" id="agi_modal_subtitle" type="text" value="<?=$agi_modal_subtitle?>" class="regular-text ">
-						</td>
-					</tr>
-					<tr id="subtitle-size">
-						<th scope="row">
-							<label for="agi_modal_subtitle">Subtitle Size</label><br />
-							<small>Make sure it is a smaller size than the title size</small>
-						</th>
-						<td>
-							<select id="agi_modal_subtitle_size" name="agi_modal_subtitle_size">
-							<?php
-								$subtitle_size_options = array(
-									'h3', 'h4', 'h5', 'h6'
-								);
-								
-								
-								foreach($subtitle_size_options as $subtitle_option) {
-									if($agi_modal_subtitle_size == $subtitle_option) {
-										$selected = " selected='selected'";
-									} else {
-										$selected = "";
+									
+								?>
+								</select>
+							</td>
+						</tr>
+						<tr id="use-subtitle">
+							<th scope="row">
+								<label for="agi_modal_use_subtitle">Use Subtitle?</label>
+							</th>
+							<td>
+								<?php $checked = ($agi_modal_use_subtitle ? 'checked' : ''); ?>
+								<input name="agi_modal_use_subtitle" id="agi_modal_use_subtitle" type="checkbox" <?=$checked?>>
+							</td>
+						</tr>
+						<tr id="subtitle">
+							<th scope="row">
+								<label for="agi_modal_subtitle">Subtitle</label>
+							</th>
+							<td>
+								<input name="agi_modal_subtitle" id="agi_modal_subtitle" type="text" value="<?=$agi_modal_subtitle?>" class="regular-text ">
+							</td>
+						</tr>
+						<tr id="subtitle-size">
+							<th scope="row">
+								<label for="agi_modal_subtitle">Subtitle Size</label><br />
+								<small>Make sure it is a smaller size than the title size</small>
+							</th>
+							<td>
+								<select id="agi_modal_subtitle_size" name="agi_modal_subtitle_size">
+								<?php
+									$subtitle_size_options = array(
+										'h3', 'h4', 'h5', 'h6'
+									);
+									
+									
+									foreach($subtitle_size_options as $subtitle_option) {
+										if($agi_modal_subtitle_size == $subtitle_option) {
+											$selected = " selected='selected'";
+										} else {
+											$selected = "";
+										}
+										echo '<option value="' . $subtitle_option . '" ' . $selected . '>&lt;' . $subtitle_option . '&gt;</option>' . "\n";
 									}
-									echo '<option value="' . $subtitle_option . '" ' . $selected . '>&lt;' . $subtitle_option . '&gt;</option>' . "\n";
-								}
-								
-							?>
-							</select>
-						</td>
-					</tr>
+									
+								?>
+								</select>
+							</td>
+						</tr>
+					</div>
 				</tbody>
 			</table>
 			<h3>Content</h3>
@@ -145,13 +156,22 @@ function agi_modal_option_page() {
 							<textarea name="agi_modal_html" id="agi_modal_html" class="large-text"><?=$agi_modal_html?></textarea>
 						</td>
 					</tr>
-					<tr id="on-pages">
+					<tr id="use_header">
 						<th scope="row">
-							<label for="agi_modal_on_pages">Show Up on Pages?</label>
+							<label for="agi_modal_use_header">Use Header</label>
 						</th>
 						<td>
-							<?php $checked = ($agi_modal_on_pages ? 'checked' : ''); ?>
-							<input name="agi_modal_on_pages" id="agi_modal_on_pages" type="checkbox" <?=$checked?>>
+							<?php $checked = ($agi_modal_use_header ? 'checked' : ''); ?>
+							<input name="agi_modal_use_header" id="agi_modal_use_header" type="checkbox" <?=$checked?>>
+						</td>
+					</tr>
+					<tr id="redirect-links">
+						<th scope="row">
+							<label for="agi_modal_redirect_links">Show Up on Pages?</label>
+						</th>
+						<td>
+							<?php $checked = ($agi_modal_redirect_links ? 'checked' : ''); ?>
+							<input name="agi_modal_redirect_links" id="agi_modal_redirect_links" type="checkbox" <?=$checked?>>
 						</td>
 					</tr>
 					<tr id="number-of-pages">
@@ -200,6 +220,15 @@ function agi_modal_option_page() {
 						</th>
 						<td>
 							<input name="agi_modal_reset_time" id="agi_modal_reset_time" type="text" value="<?=$agi_modal_reset_time?>" class="small-text"> minutes
+						</td>
+					</tr>
+					<tr id="number-of-views">
+						<th scope="row">
+							<label for="agi_modal_number_of_views">Number of Views</label><br />
+							<small>How many times do you want the modal to pop up?</small>
+						</th>
+						<td>
+							<input name="agi_modal_number_of_views" id="agi_modal_number_of_views" type="text" value="<?=$agi_modal_number_of_views?>" class="small-text"> Views
 						</td>
 					</tr>
 				</tbody>
@@ -296,6 +325,12 @@ function agi_modal_option_page() {
 			
 			
 			// Do this immediately
+			if($('#agi_modal_use_header').prop('checked')) {
+				$('#header-info').show();
+			} else {
+				$('#header-info').hide();
+			}
+			
 			if($('#agi_modal_use_subtitle').prop('checked')) {
 				$('#subtitle').show();
 				$('#subtitle-size').show();
