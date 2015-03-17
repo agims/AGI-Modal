b883044aeccdfbad6b98b18e73000a7bae0590c7
Chris G. Clapp <chris@chisosdesigns.com>
2015-03-17 12:18:50 -0600

    update version to 1.2.2

--- agi-modal.php
+++ agi-modal.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: AGI Modal
- * Description: Add a modal window that pops up when a certain point is reached.  Version 1.2.1 - Balmorhea
- * Version: 1.2.1
+ * Description: Add a modal window that pops up when a certain point is reached.  Version 1.2.2 - Balmorhea
+ * Version: 1.2.2
  * Author: Chris G. Clapp
  * Author URI: http://www.chrisclapp.com
  * Text Domain: agi-modal
